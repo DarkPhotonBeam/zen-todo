f6508769a2b1fd74fe3d519dc0a1bd6999dac4d5
Daniel Leuthold <darkphotonbeam@gmail.com>
2025-12-28 23:34:31 +0100

    fixed menu not disappearing after loggin out

--- src/app/NavMenu.tsx
+++ src/app/NavMenu.tsx
@@ -2,24 +2,41 @@
 
 import css from "./NavMenu.module.scss";
 import { House, LogOut, Menu, X } from "lucide-react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Link from "next/link";
 import { authClient } from "@/lib/auth-client";
 import { redirect } from "next/navigation";
+import { debug } from "@/lib/client-helpers";
 
 export default function NavMenu() {
   const [open, setOpen] = useState(false);
+  const [loggedIn, setLoggedIn] = useState(true);
+
+  useEffect(() => {
+    (async () => {
+      debug("useEffect NavMenu");
+      const session = await authClient.getSession();
+      if (!session) {
+        setLoggedIn(false);
+        return;
+      }
+      setLoggedIn(true);
+    })();
+  }, []);
 
   async function signOut() {
     setOpen(false);
     await authClient.signOut({
       fetchOptions: {
         onSuccess: () => {
+          setLoggedIn(false);
           redirect("/auth/sign-in");
         },
       },
     });
   }
+
+  if (!loggedIn) return null;
 
   return (
     <nav className={css.nav}>
